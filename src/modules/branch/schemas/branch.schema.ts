815796d8678f57f673e83bad54f2fe2dfe7b6271
VoDinhNghia <vodinhnghia85@gmail.com>
2022-11-26 17:06:36 +0700

    create schemas branch and center

---
+++ src/modules/branch/schemas/branch.schema.ts
@@ -0,0 +1,49 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+
+export type BranchDocument = Branch & Document;
+
+@Schema()
+export class Branch {
+  @Prop({ required: true })
+  name?: string; // Quang Ngai, HCM, Thanh Hoa
+
+  @Prop()
+  description?: string;
+
+  @Prop()
+  website?: string;
+
+  @Prop({
+    type: [
+      {
+        country: {
+          type: mongoose.Types.ObjectId,
+          ref: 'countries',
+        },
+      },
+    ],
+  })
+  location?: {
+    province?: string;
+    country?: mongoose.Types.ObjectId;
+    address?: string;
+  };
+
+  @Prop()
+  contactInfo?: [
+    {
+      email?: string;
+      fax?: string;
+      mobile?: string;
+    },
+  ];
+
+  @Prop({ default: Date.now })
+  createdAt?: Date;
+
+  @Prop({ default: Date.now })
+  updateAt?: Date;
+}
+
+export const BranchSchema = SchemaFactory.createForClass(Branch);
