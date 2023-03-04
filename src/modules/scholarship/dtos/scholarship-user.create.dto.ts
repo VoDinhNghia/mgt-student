62eb0b077cd423a97b03cb28b230d8b015f1b6d8
VoDinhNghia <vodinhnghia85@gmail.com>
2023-03-04 13:26:17 +0700

    Get list accumalated point in semester

--- src/modules/scholarship/dtos/scholarship-user.create.dto.ts
+++ src/modules/scholarship/dtos/scholarship-user.create.dto.ts
@@ -1,1 +1,15 @@
-export class CreateScholarshipSetting {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateScholarshipUser {
+  @ApiProperty({ required: true })
+  scholarship?: string;
+
+  @ApiProperty({ required: true })
+  user?: string;
+
+  @ApiProperty({ required: true })
+  point?: number;
+
+  @ApiProperty({ required: true })
+  rewardMoney?: number;
+}
