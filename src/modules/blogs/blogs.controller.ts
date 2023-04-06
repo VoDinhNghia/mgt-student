9e4f8a572f3c1905bc20ed3f5238cf300eeaa256
VoDinhNghia <vodinhnghia85@gmail.com>
2023-04-06 21:11:10 +0700

    remove blog module

--- src/modules/blogs/blogs.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
-import { BlogsService } from './blogs.service';
-
-@Controller('api/blogs')
-@ApiTags('blogs')
-export class BlogsController {
-  constructor(private readonly blogService: BlogsService) {}
-}
