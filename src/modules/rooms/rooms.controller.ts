65577188895556a2c521b8558310e77cc0f08613
VoDinhNghia <vodinhnghia85@gmail.com>
2023-09-10 09:58:45 +0700

    fix role delete room

--- src/modules/rooms/rooms.controller.ts
+++ src/modules/rooms/rooms.controller.ts
@@ -82,7 +82,7 @@ export class RoomsController {
   @Delete('/:id')
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
-  @UseGuards(RoleGuard([ErolesUser.ADMIN]))
+  @UseGuards(RoleGuard([ErolesUser.SUPPER_ADMIN, ErolesUser.ADMIN]))
   public async deleteRoom(
     @Param('id') id: string,
     @Res() res: Response,
